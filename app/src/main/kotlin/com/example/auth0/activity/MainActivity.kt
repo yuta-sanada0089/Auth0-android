c495b3078c5c09c68774c3dad2edf81c83289c8d
yuta_sanada <sndyut0089@gmail.com>
2021-12-22 23:48:20 +0900

    :+1: handle login or top

--- app/src/main/kotlin/com/example/auth0/activity/MainActivity.kt
+++ app/src/main/kotlin/com/example/auth0/activity/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.auth0.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.auth0.android.authentication.storage.CredentialsManagerException
-import com.example.auth0.service.AuthenticationService
+import androidx.lifecycle.Observer
+import com.example.auth0.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AuthenticationService(this)
-    }
+        viewModel = MainViewModel(application)
+        viewModel.fetchCredential()
 
-    private suspend fun checkCredencials() {
-        try {
-            val credencials = AuthenticationService(this).credentioals()
-        } catch (error: CredentialsManagerException) {
+        viewModel.credentialsLivedata.observe(this, Observer {
+            val intent = Intent(application, TopActivity::class.java)
+            startActivity(intent)
+        })
 
-        }
+        viewModel.errorLiveData.observe(this, Observer {
+            val intent = Intent(application, LoginActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
