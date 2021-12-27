ee4cecf4ecfbf6a42fd41a544f6c51c7209ca688
yuta_sanada <sndyut0089@gmail.com>
2021-12-27 23:56:58 +0900

    :+1: add screens

---
+++ app/src/main/kotlin/com/example/auth0/screens/LoginScreen.kt
@@ -0,0 +1,27 @@
+package com.example.auth0.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "メールアドレスでログイン")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Facebookでログイン")
+        }
+    }
+}
---
+++ app/src/main/kotlin/com/example/auth0/screens/SplashScreen.kt
@@ -0,0 +1,7 @@
+package com.example.auth0.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+}
