b42e1ad3eb9113dc129224d9cd05f0c7c62b0c29
yuta_sanada <sndyut0089@gmail.com>
2021-12-29 23:26:17 +0900

    :+1: implement SplashViewModel

--- app/src/main/kotlin/com/example/auth0/viewmodel/SplashViewModel.kt
+++ app/src/main/kotlin/com/example/auth0/viewmodel/SplashViewModel.kt
@@ -1,6 +1,29 @@
 package com.example.auth0.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.auth0.android.authentication.storage.CredentialsManagerException
+import com.auth0.android.result.Credentials
+import com.example.auth0.service.AuthenticationService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SplashViewModel: ViewModel() {
+@HiltViewModel
+class SplashViewModel @Inject constructor(private val authenticationService: AuthenticationService): ViewModel() {
+    val isLoginLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    val credentialsLivedata: MutableLiveData<Credentials> = MutableLiveData()
+
+    fun fetchCredential() {
+        viewModelScope.launch {
+            try {
+                val credentials = authenticationService.credentials()
+                credentialsLivedata.postValue(credentials)
+                isLoginLiveData.postValue(true)
+            } catch (error: CredentialsManagerException) {
+                isLoginLiveData.postValue(false)
+            }
+        }
+    }
 }
