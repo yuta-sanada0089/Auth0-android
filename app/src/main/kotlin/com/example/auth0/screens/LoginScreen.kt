a446903b8ed642e1deae7d3e959da29691d66bbd
yuta_sanada <sndyut0089@gmail.com>
2021-12-30 23:50:39 +0900

    :+1: implement LoginScreen and viewModel

--- app/src/main/kotlin/com/example/auth0/screens/LoginScreen.kt
+++ app/src/main/kotlin/com/example/auth0/screens/LoginScreen.kt
@@ -1,27 +1,55 @@
 package com.example.auth0.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import com.example.auth0.viewmodel.LoginViewModel
 
 @Composable
 fun LoginScreen(navController: NavController) {
+    val viewModel: LoginViewModel = hiltViewModel()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context: Context = LocalContext.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { /*TODO*/ }) {
+        Text(text = "LoginScreen")
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp))
+        Button(onClick = { viewModel.login(context, "Username-Password-Authentication") }) {
             Text(text = "メールアドレスでログイン")
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Facebookでログイン")
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp))
+        Button(onClick = { viewModel.login(context, "google-oauth2") }) {
+            Text(text = "Googleでログイン")
         }
     }
+    viewModel.isLoginLiveData.observe(lifecycleOwner, Observer {
+        if(it == true) {
+            navController.navigate("top")
+        } else {
+            showToastMessage(context, "login error...")
+        }
+    })
+}
+
+private fun showToastMessage(context: Context, message:String){
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/kotlin/com/example/auth0/viewmodel/LoginViewModel.kt
+++ app/src/main/kotlin/com/example/auth0/viewmodel/LoginViewModel.kt
@@ -1,26 +1,27 @@
 package com.example.auth0.viewmodel
 
-import android.app.Activity
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.auth0.android.authentication.storage.CredentialsManagerException
-import com.auth0.android.result.Credentials
 import com.example.auth0.service.AuthenticationService
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel(application: Application): AndroidViewModel(application) {
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val authenticationService: AuthenticationService): ViewModel() {
     val errorLiveData: MutableLiveData<Throwable> = MutableLiveData()
-    val credentialsLivedata: MutableLiveData<Credentials> = MutableLiveData()
-    private val authenticationService = AuthenticationService(getApplication<Application>().applicationContext)
+    val isLoginLiveData: MutableLiveData<Boolean> = MutableLiveData()
 
-    fun login(connection: String) {
+    fun login(context: Context, connection: String) {
         viewModelScope.launch {
             try {
-                val credentials = authenticationService.auth(connection)
-                credentialsLivedata.postValue(credentials)
+                val credentials = authenticationService.auth(context, connection)
+                isLoginLiveData.postValue(true)
             } catch (error: CredentialsManagerException) {
+                isLoginLiveData.postValue(false)
                 errorLiveData.postValue(error)
             }
         }
