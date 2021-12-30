831582d3b1b08f692ce7ff5468ee3944807b1c33
yuta_sanada <sndyut0089@gmail.com>
2021-12-30 23:49:08 +0900

    :+1: implement Navigation

--- app/src/main/kotlin/com/example/auth0/Navigation.kt
+++ app/src/main/kotlin/com/example/auth0/Navigation.kt
@@ -4,14 +4,22 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.auth0.screens.LoginScreen
+import com.example.auth0.screens.SplashScreen
 import com.example.auth0.screens.TopScreen
 
 @Composable
 fun ComposeNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "splash_screen") {
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") {
+            SplashScreen(navController = navController)
+        }
+        composable("login") {
+            LoginScreen(navController = navController)
+        }
         composable("top") {
-            TopScreen()
+            TopScreen(navController = navController)
         }
     }
 }
